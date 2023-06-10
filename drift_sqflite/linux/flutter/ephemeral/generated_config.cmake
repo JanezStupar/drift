39bb507f08caf9326fe1b346b249c41ecf60eac2
Janez Stupar <janez@janezstupar.com>
2023-06-10 17:28:46 +0200

    Add additional sqlite API methods to sql_crdt and sqlite_crdt necessary for Drift integration. Add desktop support for testing purposes in drift_sqflite and drift_crdt.
    Add necessary delegates.
    
    Bridge sqlite_crdt to drift_crdt.
    
    Include sql_crdt and sqlite_crdt packages for development purposes.
    
    Create a drift_crdt package.

---
+++ drift_sqflite/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/opt/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/janez/PycharmProjects/trackself/drift/drift_sqflite" PROJECT_DIR)
+
+set(FLUTTER_VERSION "2.0.1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 2 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/opt/flutter"
+  "PROJECT_DIR=/home/janez/PycharmProjects/trackself/drift/drift_sqflite"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,SU5URUdSQVRJT05fVEVTVF9TSE9VTERfUkVQT1JUX1JFU1VMVFNfVE9fTkFUSVZFPWZhbHNl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/janez/PycharmProjects/trackself/drift/drift_sqflite/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/tmp/flutter_tools.GCEGSQ/flutter_test_listener.EOAJJV/listener.dart"
+)
